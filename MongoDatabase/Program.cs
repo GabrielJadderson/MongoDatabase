420ad86132381ea40eeb9df659e7f7c4322800ae
Gabriel Jadderson <mullie34@hotmail.com>
2018-05-13 20:12:46 +0200

    initial commit

---
+++ MongoDatabase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+using MongoDB.Driver;
+
+namespace MongoDatabase
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Client = new MongoClient("mongodb://localhost:27017");
+            
+            Console.WriteLine(Client.Cluster.ClusterId.Value);
+            
+            Console.WriteLine("Works");
+            
+            Console.ReadLine();
+        }
+    }
+}
