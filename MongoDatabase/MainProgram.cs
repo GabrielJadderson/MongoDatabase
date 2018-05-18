b7c368c6fca4f82f5b3386ea6b0fcce458b5b1f1
Gabriel Jadderson <mullie34@hotmail.com>
2018-05-19 01:24:34 +0200

    LAST SHIT

--- MongoDatabase/MainProgram.cs
+++ MongoDatabase/MainProgram.cs
@@ -10,6 +10,7 @@ namespace MongoDatabase
         static void Main(string[] args)
         {
             ConfigureAndEstablishDBConnection();
+            InsertCombinations();
             new ConsoleMenu();
         }
 
@@ -23,6 +24,20 @@ namespace MongoDatabase
             Globals.UserCollection = database.GetCollection<User>("Users");
             Globals.CombinationCollection = database.GetCollection<Combination>("Combinations");
             Globals.RatingCollection = database.GetCollection<Rating>("Ratings");
+        }
+
+        public static void InsertCombinations() //for testing
+        {
+            Globals.Username = "Gabriel";
+            string[] comments = new[] {
+                "this is a good drink.","worst shit ever.","very good for getting wasted.","something wrong with this.",
+                "not the best in the world, but the best around town.","this  is heaven",
+                "one good combinatgion","can't be beaten","so good.", "Moly Holy the best gin in the world."};
+            for (int i = 0; i < 10; i++)
+            {
+                Operations.CreateCombination("gin" + i, "tonic" + i, "garnish" + i, comments[i], new Random().Next(1, 5));
+            }
+            Globals.Username = null;
         }
 
     }
